81030f3bf7a46afb7de18643ac9e561ee3a4863a
Viktor Sokolov <gzigzigzeo@evilmartians.com>
2014-12-09 01:50:05 +0300

    I18n simple spec

--- spec/lib/base_spec.rb
+++ spec/lib/base_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 
 describe TestBitfield do
   let(:storage) { { bitfield: 0 } }
+  subject { described_class.new(storage, :bitfield) }
 
   context '#initialize' do
+    let(:storage) { { bitfield: 6 } }
+
     it 'raises if too many values given' do
       expect {
         class WrongBitfield < BitfieldAttribute::Base
@@ -13,19 +16,16 @@ describe TestBitfield do
     end
 
     it 'gets initial bits from value' do
-      storage[:bitfield] = 6
-      bitfield = described_class.new(storage, :bitfield)
+      expect(subject.first).to eq(false)
+      expect(subject.first?).to eq(false)
 
-      expect(bitfield.first).to eq(false)
-      expect(bitfield.first?).to eq(false)
+      expect(subject.second).to eq(true)
+      expect(subject.second?).to eq(true)
 
-      expect(bitfield.second).to eq(true)
-      expect(bitfield.second?).to eq(true)
+      expect(subject.last).to eq(true)
+      expect(subject.last?).to eq(true)
 
-      expect(bitfield.last).to eq(true)
-      expect(bitfield.last?).to eq(true)
-
-      expect(bitfield.to_a).to eq([:second, :last])
+      expect(subject.to_a).to eq([:second, :last])
     end
   end
 
@@ -33,41 +33,52 @@ describe TestBitfield do
     it 'sets value on storage' do
       expect(storage[:bitfield]).to eq(0)
 
-      bitfield = described_class.new(storage, :bitfield)
-      bitfield.first = true
-      bitfield.last = true
+      subject.first = true
+      subject.last = true
 
-      expect(bitfield.value).to eq(5)
+      expect(subject.value).to eq(5)
       expect(storage[:bitfield]).to eq(5)
     end
 
     it 'sets values from hash' do
-      bitfield = described_class.new(storage, :bitfield)
-      bitfield.attributes = { 'first' => '1', 'second' => '0', 'last' => '1' }
+      subject.attributes = { 'first' => '1', 'second' => '0', 'last' => '1' }
 
-      expect(bitfield.value).to eq(5)
+      expect(subject.value).to eq(5)
 
-      bitfield.attributes = { first: 1 }
+      subject.attributes = { first: 1 }
 
-      expect(bitfield.value).to eq(1)
+      expect(subject.value).to eq(1)
 
-      expect(bitfield.attributes).to eq({ first: true, second: false, last: false })
+      expect(subject.attributes).to eq({ first: true, second: false, last: false })
     end
   end
 
   context '#update' do
     it 'updates attributes with provided hash' do
-      bitfield = described_class.new(storage, :bitfield)
-      bitfield.attributes = { 'first' => '1', 'second' => '0', 'last' => '1' }
+      subject.attributes = { 'first' => '1', 'second' => '0', 'last' => '1' }
 
-      bitfield.update( first: 0, second: 1 )
+      subject.update( first: 0, second: 1 )
 
-      expect(bitfield.value).to eq 6
+      expect(subject.value).to eq 6
     end
   end
 
   context 'internalization' do
+    before do
+      I18n.enforce_available_locales = false
+      I18n.backend.store_translations(I18n.default_locale,
+        activemodel: {
+          attributes: {
+            test_bitfield: {
+              first: 'First field'
+            }
+          }
+        }
+      )
+    end
 
+    it 'returns correct field name' do
+      expect(described_class.human_attribute_name(:first)).to eq('First field')
+    end
   end
 end
-
